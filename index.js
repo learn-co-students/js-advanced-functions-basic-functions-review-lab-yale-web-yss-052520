0cda56bbb92b7d68fd243237bc1f01d9fa0c8e34
Eric <eric.ou@yale.edu>
2020-07-15 14:55:12 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Your code here
+let saturdayFun = (event = "roller-skate") => {
+    return `This Saturday, I want to ${event}!`;
+}
+
+let mondayWork = (event = "go to the office") => {   
+    return `This Monday, I will ${event}.`
+}
+
+let wrapAdjective = (flair = "*") => {
+    let inner = (word = "special") => {
+        return `You are ${flair + word + flair}!`
+    };
+    return inner;
+}
+
+let Calculator = {
+    add: (a, b) => {return a + b;},
+    subtract: (a, b)=> {return a - b;},
+    multiply: (a, b)=> {return a * b;},
+    divide: (a, b)=> {return a / b;}
+};
+
+let actionApplyer = (start, fn_array) => {
+    for (const fn of fn_array){
+        start = fn(start);
+    }
+    return start;
+}
