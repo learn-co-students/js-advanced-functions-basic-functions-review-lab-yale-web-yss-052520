63a088cac5ca6c6f79dce590af95cd01c453e913
chad <chad.palmer@yale.edu>
2020-07-15 20:05:10 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Your code here
+
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+
+let mondayWork = function(activity="go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(flair="*"){
+    return function(adjective="special"){
+        return `You are ${flair}${adjective}${flair}!`
+    }
+}
+
+const Calculator = {
+    add: (a, b) => a + b,
+    subtract: (a, b) => a - b,
+    multiply: (a, b) => a * b,
+    divide: (a, b) => a / b
+}
+
+function actionApplyer(start, arr){
+    return arr.reduce((acc, f) => f(acc), start)
+}
